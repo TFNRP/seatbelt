304bd985fbcb8d7da32b5bdea6d8c28a49dca9e0
Hagen <hagen@paw.bot>
2021-11-22 00:24:44 +0000

    1.0.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 repository 'https://github.com/TFNRP/seatbelt'
 fork_repository 'https://github.com/TehRamsus/Seatbelt'
-version '0.1.3'
+version '1.0.0'
 author 'Reece Stokes <hagen@hyena.gay>'
 
 client_script {
